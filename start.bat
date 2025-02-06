2251aa721e36e4ca08e717220a94ffeef3679bd5
sreeharsh789 <singbros1234@gmail.com>
2025-02-07 00:17:03 +0530

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+python -m pip install -r requirements.txt
+python "yt_video_uploader.py"
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 -m pip install -r requirements.txt
+python3 "yt_video_uploader.py"
